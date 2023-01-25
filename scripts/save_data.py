Fixed normalizing data in save_data.py

scripts/save_data.py
@@ -52,17 +52,14 @@ for i in tqdm(range(1,38), desc="Loading Voxels"):
         curScan = beta[:, j]
         
         # Normalizing the scan.  
-        single_scan = torch.from_numpy(curScan[nsd_mask])
-        single_scan = ((single_scan - torch.mean(single_scan)) / torch.std(single_scan))
+        single_scan = curScan[nsd_mask]
+        single_scan = (single_scan - np.min(single_scan))/(np.max(single_scan)-np.min(single_scan))
+        single_scan = torch.from_numpy(single_scan)
 
         # Discard the unmasked values and keeps the masked values. 
         whole_region[j + (i-1)*750] = single_scan
-        
-# # Normalization
 
-# whole_region_mean, whole_region_std = whole_region.mean(), whole_region.std()
-# whole_region = (whole_region - whole_region_mean) / whole_region_std
-
+# Save the tensor
 torch.save(whole_region, prep_path + "x/whole_region_11838.pt")
 
 for vector in vectors: