b50ac9c5f6a7934dc75704e1e90df2f00a9b2ce7
Jordyn Ojeda <jordynaojeda@gmail.com>
2023-01-26 13:59:48 -0600

    Added grid image reconstruction feature

--- src/driver.py
+++ src/driver.py
@@ -3,6 +3,7 @@ os.environ['CUDA_VISIBLE_DEVICES'] = "2,3"
 import torch
 from torch.autograd import Variable
 import numpy as np
+from PIL import Image
 from nsd_access import NSDAccess
 import glob
 import pandas as pd
@@ -20,8 +21,15 @@ from decoder import Decoder
 
 def main():
     os.chdir("/export/raid1/home/kneel027/Second-Sight/")
+
+    
+    reconstructNImages(z_model_hash="044",
+                         c_model_hash="022",
+                         idx=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+
+
+def train_decoder():
     hashNum = update_hash()
-    hashNum = "022"
     D = Decoder(hashNum = hashNum,
                  lr=0.00001,
                  vector="c", 
@@ -33,7 +41,7 @@ def main():
                  num_workers=16,
                  epochs=300
                  )
-    # D.train()
+    D.train()
     modelId = D.hashNum + "_model_" + D.vector + ".pt"
     outputs_c, targets_c = D.predict(model=modelId, indices=[1, 2, 3])
     # Test
@@ -42,30 +50,122 @@ def main():
     cosSim = nn.CosineSimilarity(dim=0)
     print(cosSim(outputs_c[0], targets_c[0]))
     print(cosSim(torch.randn_like(outputs_c[0]), targets_c[0]))
+
+# Encode latent z (1x4x64x64) and condition c (1x77x1024) tensors into an image
+# Strength parameter controls the weighting between the two tensors
+def reconstructNImages(z_model_hash, c_model_hash, idx):
+    Dz = Decoder(hashNum = z_model_hash,
+                 vector="z", 
+                 log=False, 
+                 device="cuda",
+                 )
+    Dc = Decoder(hashNum = c_model_hash,
+                 vector="c", 
+                 log=False, 
+                 device="cuda",
+                 )
+    
+    # First URL: This is the original read-only NSD file path (The actual data)
+    # Second URL: Local files that we are adding to the dataset and need to access as part of the data
+    # Object for the NSDAccess package
+    nsda = NSDAccess('/home/naxos2-raid25/kneel027/home/surly/raid4/kendrick-data/nsd', '/home/naxos2-raid25/kneel027/home/kneel027/nsd_local')
+    
+    # Retriving the ground truth image. 
+    subj1 = nsda.stim_descriptions[nsda.stim_descriptions['subject1'] != 0]
+        
+
+    # Grabbing the models for a hash
+    z_modelId = z_model_hash + "_model_" + Dz.vector + ".pt"
+    c_modelId = c_model_hash + "_model_" + Dc.vector + ".pt"
+    
+    # Generating predicted and target vectors
+    outputs_c, targets_c = Dc.predict(model=c_modelId, indices=idx)
+    outputs_z, targets_z = Dz.predict(model=z_modelId, indices=idx)
+    strength_c = 0.999999999999
+    strength_z = 0.000000000001
     
     E = Encoder()
-    z = torch.load("/export/raid1/home/kneel027/Second-Sight/latent_vectors/" + "044_model_z.pt" + "/target_z.pt")
-    c = torch.load("/export/raid1/home/kneel027/Second-Sight/latent_vectors/" + "022_model_c.pt" + "/target_c.pt")
-    outputs_z = torch.load("/export/raid1/home/kneel027/Second-Sight/latent_vectors/" + "044_model_z.pt" + "/output_z.pt")
-    E.reconstructNImages(z, outputs_c[0], targets_c[0],  0.9999999999,
-                         c, outputs_z[0], z,  0.00000000001)
-
-    # img = E.reconstruct(z, outputs[0], 0.9999999999)
-    # img2 = E.reconstruct(z, targets[0], 0.9999999999)
-    # img = E.reconstruct(z, outputs[1], 0.9999999999)
-    # img2 = E.reconstruct(z, targets[1], 0.9999999999)
-    # img = E.reconstruct(z, outputs[2], 0.9999999999)
-    # img2 = E.reconstruct(z, targets[2], 0.9999999999)
     
-    # reconstruction_outputs = [outputs[0], outputs[1], outputs[2], outputs[3]]
-    # reconstruction_targets = [targets[0], targets[1], targets[2], targets[3]]
-    # c = torch.load("/export/raid1/home/kneel027/Second-Sight/latent_vectors/" + "022_model_c.pt" + "/target_c.pt")
-    # img = E.reconstruct(outputs[0], c, 0.00000000001)
-    # img2 = E.reconstruct(targets[0], c, 0.00000000001)
-    # img = E.reconstruct(outputs[1], c, 0.00000000001)
-    # img2 = E.reconstruct(targets[1], c, 0.00000000001)
-    # img = E.reconstruct(outputs[2], c, 0.00000000001)
-    # img2 = E.reconstruct(targets[2], c, 0.00000000001)
+    for i in range(len(idx)):
+        print(i)
+        test_i = idx[i] + 25501
+        
+        # Make the c reconstrution images. 
+        print(len(targets_z), targets_z[i].shape, len(targets_c), targets_c[i].shape)
+        
+        reconstructed_output_c = E.reconstruct(targets_z[i], outputs_c[i], strength_c)
+        reconstructed_target_c = E.reconstruct(targets_z[i], targets_c[i], strength_c)
+        
+        # Make the z reconstrution images. 
+        reconstructed_output_z = E.reconstruct(outputs_z[i], targets_c[i], strength_z)
+        reconstructed_target_z = E.reconstruct(targets_z[i], targets_c[i], strength_z)
+        
+        # Make the z and c reconstrution images. 
+        z_c_reconstruction = E.reconstruct(outputs_z[i], outputs_c[i], 0.9)
+        
+        index = int(subj1.loc[(subj1['subject1_rep0'] == test_i) | (subj1['subject1_rep1'] == test_i) | (subj1['subject1_rep2'] == test_i)].nsdId)
 
+        # returns a numpy array 
+        ground_truth_np_array = nsda.read_images([index], show=True)
+        ground_truth = Image.fromarray(ground_truth_np_array[0])
+        
+        # Create figure
+        fig = plt.figure(figsize=(10, 7))
+        plt.title(str(i) + ": Reconstruction")
+        
+        # Setting values to rows and column variables
+        rows = 3
+        columns = 2
+        
+        # Adds a subplot at the 1st position
+        fig.add_subplot(rows, columns, 1)
+        
+        # Showing image
+        plt.imshow(ground_truth)
+        plt.axis('off')
+        plt.title("Ground Truth")
+        
+        # Adds a subplot at the 2nd position
+        fig.add_subplot(rows, columns, 2)
+        
+        # Showing image
+        plt.imshow(z_c_reconstruction)
+        plt.axis('off')
+        plt.title("Z and C Reconstructed")
+        
+        # Adds a subplot at the 1st position
+        fig.add_subplot(rows, columns, 3)
+        
+        # Showing image
+        plt.imshow(reconstructed_output_c)
+        plt.axis('off')
+        plt.title("Reconstructed Output C")
+        
+        # Adds a subplot at the 2nd position
+        fig.add_subplot(rows, columns, 4)
+        
+        # Showing image
+        plt.imshow(reconstructed_target_c)
+        plt.axis('off')
+        plt.title("Reconstructed Target C")
+        
+        # Adds a subplot at the 3rd position
+        fig.add_subplot(rows, columns, 5)
+        
+        # Showing image
+        plt.imshow(reconstructed_output_z)
+        plt.axis('off')
+        plt.title("Reconstructed Output Z")
+        
+        # Adds a subplot at the 4th position
+        fig.add_subplot(rows, columns, 6)
+        
+        # Showing image
+        plt.imshow(reconstructed_target_z)
+        plt.axis('off')
+        plt.title("Reconstructed Target Z")
+        
+        plt.savefig('reconstructions/' + str(i) + '_reconstruction.png')
+    
 if __name__ == "__main__":
     main()
--- src/encoder.py
+++ src/encoder.py
@@ -22,7 +22,6 @@ from stablediffusion.ldm.models.diffusion.ddim import DDIMSampler
 
 class Encoder():
     def __init__(self):
-        seed_everything(42)
         self.config = OmegaConf.load("stablediffusion/configs/stable-diffusion/v2-inference.yaml")
         self.model = self.load_model_from_config(self.config, "stablediffusion/models/v2-1_512.ckpt")
         
@@ -31,8 +30,8 @@ class Encoder():
         self.scale = 9.0
         self.sampler = DDIMSampler(self.model)
 
-        os.makedirs("reconstructions/", exist_ok=True)
-        self.outpath = "reconstructions/"
+        os.makedirs("reconstructions/samples", exist_ok=True)
+        self.outpath = "reconstructions/samples"
         self.base_count = len(os.listdir(self.outpath))
         
     def load_model_from_config(self, config, ckpt, verbose=False):
@@ -58,6 +57,7 @@ class Encoder():
     # Encode latent z (1x4x64x64) and condition c (1x77x1024) tensors into an image
     # Strength parameter controls the weighting between the two tensors
     def reconstruct(self, z, c, strength=0.8):
+        # seed_everything(41)
         init_latent = z.reshape((1,4,64,64)).to(self.device)
         self.sampler.make_schedule(ddim_num_steps=50, ddim_eta=0.0, verbose=False)
 
@@ -86,95 +86,7 @@ class Encoder():
                     self.base_count += 1
         return img
     
-    # Encode latent z (1x4x64x64) and condition c (1x77x1024) tensors into an image
-    # Strength parameter controls the weighting between the two tensors
-    def reconstructNImages(self, z, output_c, target_c, strength_c,
-                                 c, output_z, target_z, strength_z=0.8,
-                                 reconstruction_strength=0.8):
-        
-        
-        reconstructed_output_c = self.reconstruct(z, output_c[0], strength_c)
-        reconstructed_target_c = self.reconstruct(z, target_c[0], strength_c)
-        reconstructed_output_z  = self.reconstruct(output_z, c , strength_z)
-        reconstructed_target_z  = self.reconstruct(target_z, c , strength_z)
-        z_c_reconstruction = self.reconstruct(output_z, output_c[0], reconstruction_strength)
-        
-        
-        # self.reconstruct()
-        # self.reconstruct()
-        
-        #imgs = [reconstructed_c, ground_truth_c, reconstructed_c_1, ground_truth_c_1]
-        
-        # First URL: This is the original read-only NSD file path (The actual data)
-        # Second URL: Local files that we are adding to the dataset and need to access as part of the data
-        # Object for the NSDAccess package
-        # nsda = NSDAccess('/home/naxos2-raid25/kneel027/home/surly/raid4/kendrick-data/nsd', '/home/naxos2-raid25/kneel027/home/kneel027/nsd_local')
-        
-        # # Retriving the ground truth image. 
-        # subj1 = nsda.stim_descriptions[nsda.stim_descriptions['subject1'] != 0]
-        # i=27750
-        # index = int(subj1.loc[(subj1['subject1_rep0'] == i) | (subj1['subject1_rep1'] == i) | (subj1['subject1_rep2'] == i)].nsdId)
-        # print(index)
-        # img = nsda.read_images([index], show=True)
-        
-        # create figure
-        fig = plt.figure(figsize=(10, 7))
-        
-        # setting values to rows and column variables
-        rows = 3
-        columns = 2
-        
-        # Adds a subplot at the 1st position
-        fig.add_subplot(rows, columns, 1)
-        
-        # showing image
-        plt.imshow(z_c_reconstruction)
-        plt.axis('off')
-        plt.title("Ground Truth")
-        
-        # Adds a subplot at the 2nd position
-        fig.add_subplot(rows, columns, 2)
-        
-        # showing image
-        plt.imshow(z_c_reconstruction)
-        plt.axis('off')
-        plt.title("Z and C Reconstructed")
-        
-        
-        # Adds a subplot at the 1st position
-        fig.add_subplot(rows, columns, 3)
-        
-        # showing image
-        plt.imshow(reconstructed_output_c)
-        plt.axis('off')
-        plt.title("Reconstructed Output C")
-        
-        # Adds a subplot at the 2nd position
-        fig.add_subplot(rows, columns, 4)
-        
-        # showing image
-        plt.imshow(reconstructed_target_c)
-        plt.axis('off')
-        plt.title("Reconstructed Target C")
-        
-        # Adds a subplot at the 3rd position
-        fig.add_subplot(rows, columns, 5)
-        
-        # showing image
-        plt.imshow(reconstructed_output_z)
-        plt.axis('off')
-        plt.title("Reconstructed Output Z")
-        
-        # Adds a subplot at the 4th position
-        fig.add_subplot(rows, columns, 6)
-        
-        # showing image
-        plt.imshow(reconstructed_target_z)
-        plt.axis('off')
-        plt.title("Reconstructed Target Z")
-        
-        plt.show()
-        plt.savefig('FourImageReconstruction.png')
+
 
     if __name__ == "__main__":
         z = torch.load(sys.argv[1])
