1cb0e5ddec45fc4137241a54c9cef16d47e37115
Reese Kneeland <reesekneeland@gmail.com>
2023-04-06 12:48:20 -0500

    Revert "merging"
    This reverts commit d26d7f7104ace67a25a5ba586d3d3dc8e9ef273f, reversing
    changes made to 354c2a1969bfc6514a77704f649162d85435dc52.

--- src/utils.py
+++ src/utils.py
@@ -562,26 +562,7 @@ def get_value(_x):
 def set_value(_x, x):
     if list(x.shape)!=list(_x.size()):
         _x.resize_(x.shape)
-    _x.data.copy_(torch.from_numpy(x))\
-        
-        
-# SCS Performance Metrics
-
-def mse_scs(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
-	
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
-
-def ssim_scs(imageA, imageB):
-    return ssim(imageA, imageB)
-    
-
+    _x.data.copy_(torch.from_numpy(x))
     
 def equalize_color(image):
     filt = ImageEnhance.Color(image)
